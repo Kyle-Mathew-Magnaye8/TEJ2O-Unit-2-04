59dce6ef816e6fd9ccc33c86c1101b23225ecf7b
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2026-02-19 08:09:31 -0500

    updated program 6 sided dice

--- main.ts
+++ main.ts
@@ -1,8 +1,18 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Kyle Matthew
+ * Created on: Feb 2026
+ * This program simulates a 6 sided dice.
 */
 
-basic.showString('Hello, World!')
+// our variables
+let randomNumber: number
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.pause(1000)
+
+input.onButtonPressed(Button.A, function() {
+  randomNumber = randint(1, 6)
+  basic.showNumber(randomNumber)
+})
